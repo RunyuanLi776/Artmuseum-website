436587e850cb77bce822be4edcd3b3c557c1ca2f
RuyuanLi <li.runyu@husky.neu.edu>
2020-03-15 00:48:00 -0400

    update package.json

--- app.js
+++ app.js
@@ -22,8 +22,23 @@ app.use(bodyParser.json());
 
 const path = require('path');
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist'));
 });
+
+const forceSSL = function() {
+        return function(req, res, next) {
+            if (req.headers['x-forwarded-proto'] !== 'https') {
+                return res.redirect(
+                    ['https://', req.get('Host'), req.url].join('')
+                );
+            }
+            next();
+        }
+    }
+    // Instruct the app
+    // to use the forceSSL
+    // middleware
+app.use(forceSSL());
 
 //Enabling CORS
 app.use(function(req, res, next) {
